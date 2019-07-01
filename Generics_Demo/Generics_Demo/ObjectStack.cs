31c0df8027da2206f95f6a396e7563ec9138ffe8
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-01 11:21:03 +0200

    ObjectStack

---
+++ Generics_Demo/Generics_Demo/ObjectStack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Demo
+{
+    class ObjectStack
+    {
+        public ObjectStack() : this(4){}
+        public ObjectStack(int capacity)
+        {
+            data = new object[capacity];
+            position = 0;
+        }
+
+        private int position;
+        private object[] data;
+
+
+        public void Push(object item)
+        {
+            if(data.Length == position)
+            {
+                object[] newData = new object[data.Length * 2];
+                Array.Copy(data, newData, data.Length);
+                data = newData;
+            }
+
+            data[position++] = item;
+            //position++;
+        }
+
+        public object Pop()
+        {
+            if (position == 0)
+                throw new InvalidOperationException("Stack ist leer");
+
+            // position--;
+            object result =  data[--position];
+            data[position] = null; // referenz löschen
+            return result;
+        }
+
+        // Push -> speichern
+        // Pop -> herausnehmen
+
+        // optional: Find
+    }
+}
--- Generics_Demo/Generics_Demo/Program.cs
+++ Generics_Demo/Generics_Demo/Program.cs
@@ -10,6 +10,44 @@ namespace Generics_Demo
     {
         static void Main(string[] args)
         {
+            #region Pre und Postincrement
+            //int zahl1 = 42;
+
+            //int zahl2 = zahl1++;
+
+            //Console.WriteLine(zahl1);
+            //Console.WriteLine(zahl2);
+
+            //Console.WriteLine("---Pre----");
+
+            //zahl1 = 42;
+
+            //zahl2 = ++zahl1;
+
+            //Console.WriteLine(zahl1);
+            //Console.WriteLine(zahl2); 
+            #endregion
+
+
+            ObjectStack os = new ObjectStack();
+
+            os.Push("12");
+            os.Push(12345);
+            os.Push(12.5);
+            os.Push("Hallo Welt");
+
+            os.Push("größer !!!");
+
+            Console.WriteLine(os.Pop());
+            Console.WriteLine(os.Pop());
+            Console.WriteLine(os.Pop());
+            Console.WriteLine(os.Pop());
+            Console.WriteLine(os.Pop());
+
+            // Console.WriteLine(os.Pop()); // Exception
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
