64c4b111201036d9e834c78d89c99a5129e2b234
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-02 10:29:37 +0200

    OpenClosed - Prinzip

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SOLID_Taschenrechner
@@ -14,7 +15,7 @@ namespace SOLID_Taschenrechner
             // IoC - Container (Unity, Autofac, CastleWindsor)
 
             var parser = new StringSplitParser();
-            var rechner = new SimplerRechner();
+            var rechner = new ModulRechner(new Addition());
             new KonsolenUI(parser,rechner).Start();
         }
     }
@@ -51,6 +52,28 @@ namespace SOLID_Taschenrechner
         }
     }
 
+    public class RegexParser : IParser
+    {
+        public Formel Parse(string input)
+        {
+            Regex r = new Regex(@"(\d+)\s*([+\-*/])\s*(\d+)",RegexOptions.Compiled);
+            var result = r.Match(input);
+
+            if (result.Success)
+            {
+                Formel formel = new Formel();
+
+                formel.Operand1 = Convert.ToInt32(result.Groups[1].Value);
+                formel.Rechenoperator = result.Groups[2].Value;
+                formel.Operand2 = Convert.ToInt32(result.Groups[3].Value);
+
+                return formel;
+            }
+            else
+                throw new FormatException("Ungültiges Formelformat");
+        }
+    }
+
     public interface IRechner
     {
         int Berechne(Formel formel);
@@ -66,6 +89,42 @@ namespace SOLID_Taschenrechner
             else
                 throw new InvalidOperationException("Der Rechenoperator ist unbekannt");
         }
+    }
+
+    public class ModulRechner : IRechner
+    {
+        public ModulRechner(params IRechenart[] rechenarten)
+        {
+            this.rechenarten = rechenarten;
+        }
+        private readonly IRechenart[] rechenarten;
+
+        public int Berechne(Formel formel)
+        {
+            var rechenart = rechenarten.FirstOrDefault(x => x.Rechenoperator == formel.Rechenoperator);
+            if (rechenart != null)
+                return rechenart.Berechne(formel.Operand1, formel.Operand2);
+            else
+                throw new InvalidOperationException($"Operator '{formel.Rechenoperator}'ist leider unbekannt");
+        }
+    }
+
+    public interface IRechenart
+    {
+        string Rechenoperator { get; }
+        int Berechne(params int[] operanden);
+    }
+    public class Addition : IRechenart
+    {
+        public string Rechenoperator => "+";
+
+        public int Berechne(params int[] operanden) => operanden.Sum();
+    }
+    public class Subtraktion : IRechenart
+    {
+        public string Rechenoperator => "-";
+
+        public int Berechne(params int[] operanden) => operanden[0] - operanden.Skip(1).Sum();
     }
 
     public class KonsolenUI
