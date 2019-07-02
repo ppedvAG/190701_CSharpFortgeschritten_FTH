47e75c58c8f08d6f496dfc918497a97afc2f773d
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-02 09:34:29 +0200

    Grundfunktion ohne SOLID

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -10,6 +10,24 @@ namespace SOLID_Taschenrechner
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Bitte geben Sie die Formel ein:"); // "2 + 2"
+            string eingabe = Console.ReadLine();
+
+            string[] teile = eingabe.Split();
+            int zahl1 = Convert.ToInt32(teile[0]);
+            string op = teile[1];
+            int zahl2 = Convert.ToInt32(teile[2]);
+
+            int ergebnis = 0;
+            if (op == "+")
+                ergebnis = zahl1 + zahl2;
+            else if (op == "-")
+                ergebnis = zahl1 - zahl2;
+
+            Console.WriteLine($"Das Ergebnis ist: {ergebnis}");
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
