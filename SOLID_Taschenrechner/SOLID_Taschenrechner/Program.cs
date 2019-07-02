a97eb8173299715f9a7752d8914baf886cf1a723
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-02 09:42:49 +0200

    Version 1 mit SingleResponsibility-Prinzip

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -8,21 +8,67 @@ namespace SOLID_Taschenrechner
 {
     class Program
     {
+        // Bootstrapping => Alles konfigurieren und initialisieren
         static void Main(string[] args)
         {
-            Console.WriteLine("Bitte geben Sie die Formel ein:"); // "2 + 2"
-            string eingabe = Console.ReadLine();
+            new KonsolenUI().Start();
+        }
+    }
 
-            string[] teile = eingabe.Split();
+    public struct Formel
+    {
+        public Formel(int operand1, int operand2, string rechenoperator)
+        {
+            Operand1 = operand1;
+            Operand2 = operand2;
+            Rechenoperator = rechenoperator;
+        }
+
+        public int Operand1 { get; set; }
+        public int Operand2 { get; set; }
+        public string Rechenoperator { get; set; }
+    }
+
+    public class StringSplitParser
+    {
+        public Formel Parse(string input)
+        {
+            string[] teile = input.Split();
             int zahl1 = Convert.ToInt32(teile[0]);
             string op = teile[1];
             int zahl2 = Convert.ToInt32(teile[2]);
 
-            int ergebnis = 0;
-            if (op == "+")
-                ergebnis = zahl1 + zahl2;
-            else if (op == "-")
-                ergebnis = zahl1 - zahl2;
+            return new Formel(zahl1, zahl2, op);
+        }
+    }
+
+    public class SimplerRechner
+    {
+        public int Berechne(Formel formel)
+        {
+            if (formel.Rechenoperator == "+")
+                return formel.Operand1 + formel.Operand2;
+            else if (formel.Rechenoperator == "-")
+                return formel.Operand1 - formel.Operand2;
+            else
+                throw new InvalidOperationException("Der Rechenoperator ist unbekannt");
+        }
+    }
+
+    public class KonsolenUI
+    {
+        public void Start()
+        {
+            Console.WriteLine("Bitte geben Sie die Formel ein:"); // "2 + 2"
+            string eingabe = Console.ReadLine();
+
+            // Parsen
+            StringSplitParser parser = new StringSplitParser();
+            Formel f = parser.Parse(eingabe);
+
+            // Rechnen
+            SimplerRechner rechner = new SimplerRechner();
+            int ergebnis = rechner.Berechne(f);
 
             Console.WriteLine($"Das Ergebnis ist: {ergebnis}");
 
