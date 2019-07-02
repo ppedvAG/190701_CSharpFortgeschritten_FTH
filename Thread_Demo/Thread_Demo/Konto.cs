cb8b5dbeb28d3e18f2e62272976076b7e4d13b86
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-02 15:14:34 +0200

    Monitor und Lock-Block

--- Thread_Demo/Thread_Demo/Konto.cs
+++ Thread_Demo/Thread_Demo/Konto.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Thread_Demo
@@ -11,25 +12,33 @@ namespace Thread_Demo
         public Konto(decimal kontostand)
         {
             Kontostand = kontostand;
-            buchungnummer = 1;
+            buchungnummer = 0;
         }
 
+        private static object lockObject = new object();
         private static int buchungnummer;
         public decimal Kontostand { get; private set; }
 
         public void Einzahlen(decimal betrag)
         {
+            Monitor.Enter(lockObject);
+
             buchungnummer++;
             Console.WriteLine($"[{buchungnummer}] Einzahlen wird gestartet: Alter Kontostand: {Kontostand:C2}, Betrag: {betrag:C2}");
             Kontostand += betrag;
             Console.WriteLine($"[{buchungnummer}] Einzahlen erfolgreich: Neuer Kontostand: {Kontostand:C2}, Betrag: {betrag:C2}");
+
+            Monitor.Exit(lockObject);
         }
         public void Abheben(decimal betrag)
         {
-            buchungnummer++;
-            Console.WriteLine($"[{buchungnummer}] Abheben wird gestartet: Alter Kontostand: {Kontostand:C2}, Betrag: {betrag:C2}");
-            Kontostand -= betrag;
-            Console.WriteLine($"[{buchungnummer}] Abheben erfolgreich: Neuer Kontostand: {Kontostand:C2}, Betrag: {betrag:C2}");
+            lock (lockObject)
+            {
+                buchungnummer++;
+                Console.WriteLine($"[{buchungnummer}] Abheben wird gestartet: Alter Kontostand: {Kontostand:C2}, Betrag: {betrag:C2}");
+                Kontostand -= betrag;
+                Console.WriteLine($"[{buchungnummer}] Abheben erfolgreich: Neuer Kontostand: {Kontostand:C2}, Betrag: {betrag:C2}");
+            }
         }
 
 
