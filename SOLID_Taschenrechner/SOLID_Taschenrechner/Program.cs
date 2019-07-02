6d1b9d35e763810e129ff63eac5da728da054154
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-02 09:51:45 +0200

    DI - Prinzip: Klassen werden über Main in das KonsolenUI eingefügt

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -11,7 +11,11 @@ namespace SOLID_Taschenrechner
         // Bootstrapping => Alles konfigurieren und initialisieren
         static void Main(string[] args)
         {
-            new KonsolenUI().Start();
+            // IoC - Container (Unity, Autofac, CastleWindsor)
+
+            var parser = new StringSplitParser();
+            var rechner = new SimplerRechner();
+            new KonsolenUI(parser,rechner).Start();
         }
     }
 
@@ -29,7 +33,12 @@ namespace SOLID_Taschenrechner
         public string Rechenoperator { get; set; }
     }
 
-    public class StringSplitParser
+    public interface IParser
+    {
+        Formel Parse(string input);
+    }
+
+    public class StringSplitParser : IParser
     {
         public Formel Parse(string input)
         {
@@ -42,7 +51,11 @@ namespace SOLID_Taschenrechner
         }
     }
 
-    public class SimplerRechner
+    public interface IRechner
+    {
+        int Berechne(Formel formel);
+    }
+    public class SimplerRechner : IRechner
     {
         public int Berechne(Formel formel)
         {
@@ -57,20 +70,35 @@ namespace SOLID_Taschenrechner
 
     public class KonsolenUI
     {
+        public KonsolenUI(IParser parser, IRechner rechner)
+        {
+            this.parser = parser;
+            this.rechner = rechner;
+        }
+
+        private readonly IParser parser;
+        private readonly IRechner rechner;
+
         public void Start()
         {
-            Console.WriteLine("Bitte geben Sie die Formel ein:"); // "2 + 2"
-            string eingabe = Console.ReadLine();
+            bool tryAgain = false;
+            do
+            {
+                Console.WriteLine("Bitte geben Sie die Formel ein:"); // "2 + 2"
+                string eingabe = Console.ReadLine();
 
-            // Parsen
-            StringSplitParser parser = new StringSplitParser();
-            Formel f = parser.Parse(eingabe);
+                // Parsen
+                Formel f = parser.Parse(eingabe);
 
-            // Rechnen
-            SimplerRechner rechner = new SimplerRechner();
-            int ergebnis = rechner.Berechne(f);
+                // Rechnen
+                int ergebnis = rechner.Berechne(f);
 
-            Console.WriteLine($"Das Ergebnis ist: {ergebnis}");
+                Console.WriteLine($"Das Ergebnis ist: {ergebnis}");
+
+                Console.Write("Wollen Sie das Programm beenden ? (q)");
+                tryAgain = Console.ReadKey().KeyChar == 'q' ? false : true;
+                Console.Clear();
+            } while (tryAgain);
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
