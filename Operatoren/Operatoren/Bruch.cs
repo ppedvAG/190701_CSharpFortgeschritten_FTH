d2e6ffcc8a65e22e4e718d196a298ea3808a2ee8
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-03 14:28:50 +0200

    Operatoren

---
+++ Operatoren/Operatoren/Bruch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatoren
+{
+    class Bruch
+    {
+        public Bruch(int zähler, int nenner)
+        {
+            Zähler = zähler;
+            Nenner = nenner;
+        }
+
+        public int Zähler { get; set; }
+        public int Nenner { get; set; }
+
+        public static Bruch operator*(Bruch links, Bruch rechts)
+        {
+            return new Bruch(links.Zähler * rechts.Zähler, links.Nenner * rechts.Nenner);
+        }
+        public static Bruch operator *(Bruch links, int rechts)
+        {
+            return new Bruch(links.Zähler * rechts, links.Nenner * 1);
+        }
+        public static Bruch operator /(Bruch links, Bruch rechts)
+        {
+            return new Bruch(links.Zähler * rechts.Nenner, links.Nenner * rechts.Zähler);
+        }
+
+        // Operatoren:
+        // +,-,*,/,%, <<, >>, &, |, ^
+        // +=, -= etc.. 
+
+        // Vergleichsoperatoren
+        // Achtung: Paarweise implementieren
+        // <,> <=,>=, ==, !=
+
+    }
+}
--- Operatoren/Operatoren/Program.cs
+++ Operatoren/Operatoren/Program.cs
@@ -10,6 +10,17 @@ namespace Operatoren
     {
         static void Main(string[] args)
         {
+            Bruch b1 = new Bruch(1, 2);
+            Bruch b2 = new Bruch(1, 4);
+
+            Bruch ergebnis = b1 * b2;
+            Bruch ergebnis2 = b1 / b2;
+
+            Bruch ergebnis3 = b1 * 3;
+
+            Console.WriteLine($"Ergebnis: {ergebnis.Zähler}/{ergebnis.Nenner}");
+            Console.WriteLine($"Ergebnis: {ergebnis2.Zähler}/{ergebnis2.Nenner}");
+            Console.WriteLine($"Ergebnis: {ergebnis3.Zähler}/{ergebnis3.Nenner}");
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
